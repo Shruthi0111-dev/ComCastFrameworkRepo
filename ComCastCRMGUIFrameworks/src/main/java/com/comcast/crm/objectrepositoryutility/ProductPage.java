9a005676e8f25541274e2170372e73dca9860235
admin <admin@DESKTOP-8R7K7D8.mshome.net>
2025-01-28 11:33:38 +0530

    added new pom class

--- ComCastCRMGUIFrameworks/src/main/java/com/comcast/crm/objectrepositoryutility/ProductPage.java
+++ ComCastCRMGUIFrameworks/src/main/java/com/comcast/crm/objectrepositoryutility/ProductPage.java
@@ -1,8 +1,19 @@
 package com.comcast.crm.objectrepositoryutility;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class ProductPage {
 
-	FindBy
+	public ProductPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//img[@title='Create Product...']")
+	private WebElement createProductImgBtn;
+	
+	
 }
